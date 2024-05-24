8e640867aa2d35e32bb9bb5a3f0b6e8c69df0c04
Jordan Haddadi <jordanhaddadi@Jordans-MacBook-Pro.local>
2024-05-24 12:48:58 -0600

    Drawer, hamburger, adding state

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,21 @@
+<script setup>
+import {ref} from 'vue'
+const drawer = ref(false)
+</script>
+
 <template>
     <v-layout class="rounded rounded-md">
-      <v-app-bar title="Application bar">
+      <v-app-bar>
+        <template #prepend>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+                <v-icon>mdi-menu</v-icon>
+            </v-app-bar-nav-icon>
+            <v-toolbar-title>
+                <router-link to="/" class="text-decoration-none">
+                    My Awesome App
+                </router-link>
+            </v-toolbar-title>
+        </template>
         <template #append>
             <v-menu>
                 <template v-slot:activator="{ props }">
@@ -16,19 +31,34 @@
                     v-for="(item, index) in ['Dashboard', 'Settings', 'Help', 'About', 'Logout']"
                     :key="index"
                     :value="index"
-                    v-bind="props"
                     >
                     <v-list-item-title>{{ item }}</v-list-item-title>
                     </v-list-item>
                 </v-list>
             </v-menu>
-
         </template>
       </v-app-bar>
   
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item title="Navigation drawer"></v-list-item>
+      <!-- Navigation drawer -->
+      <v-navigation-drawer
+        v-model="drawer"
+        expand-on-hover="true"
+        rail
+      >
+   
+          <template v-slot:append>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </template>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-note-multiple-outline" title="Posts" value="posts" :to="'/posts'"></v-list-item>
+          <v-list-item prepend-icon="mdi-comment-multiple-outline" title="Comments" value="comments" :to="'/comments'"></v-list-item>
         </v-list>
       </v-navigation-drawer>
   
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 // Composables
 import Default from "@/layouts/Default.vue";
 import HelloWorld from '@/pages/Home.vue';
+import Posts from '@/pages/Posts.vue';
+import Comments from '@/pages/Comments.vue';
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
@@ -15,6 +17,22 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: HelloWorld,
+      },
+      {
+        path: '/posts',
+        name: "Posts",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: Posts,
+      },
+      {
+        path: '/comments',
+        name: "Comments",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: Comments,
       }
     ],
   },
